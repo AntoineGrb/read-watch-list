27307628ccdca0475b5352e5cf4a43ff5d425768
Antoine Grubert <antoine_grubert@hotmail.fr>
2023-05-04 10:45:55 +0200

    V3 - appli ok , reste tests bugs + dark mode

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 import Sortable from './node_modules/sortablejs/modular/sortable.complete.esm.js';
 
 //#region VARIABLES
-    //Boutons de sélection
-    const btnListOn = document.querySelector(".btn__list--on");
-    const btnListCompleted = document.querySelector(".btn__list--completed");
-    const btnListAll = document.querySelector(".btn__filters--all");
-    const btnListBooks = document.querySelector(".btn__filters--film");
-    const btnListFilm = document.querySelector(".btn__filters--film");
-    const btnListTv = document.querySelector(".btn__filters--tvshows");
-
     //Barre ajout tâche
     const inputAdd = document.querySelector("#input__add");
     const btnAdd = document.querySelector("#btn__add");
@@ -20,7 +12,6 @@ import Sortable from './node_modules/sortablejs/modular/sortable.complete.esm.js
     const listTitle = document.querySelector(".list__title > h2");
 
     //Déclaration des items
-    let items = [];
     class Item {
         constructor(id, name, theme, checked) {
             this.id = id;
@@ -29,11 +20,28 @@ import Sortable from './node_modules/sortablejs/modular/sortable.complete.esm.js
             this.checked = checked;
         }
     };
+
+    //Valeurs par défaut 
+    let btnListActive = "On the list";
+    let btnFilterActive = "All";
+
+//#endregion
+
+//#region CHARGEMENT DES DONNEES FROM LOCAL STORAGE
+    let items = window.localStorage.getItem("items");
+    if (items === null) {
+        items = [];
+    }
+    else {
+        items = JSON.parse(items);
+        updateList();
+    }
 //#endregion
 
 //#region CREER UN NOUVEL ITEM 
     //Fonction : Créer le nouvel objet item
     async function addItem(items, itemTheme, animate) {
+        //Déterminer l'id à partir de ids existants dans le tableau
         let idMax = 0;
         for (let i=0 ; i < items.length ; i++) {
             if (items[i].id > idMax) {
@@ -42,26 +50,28 @@ import Sortable from './node_modules/sortablejs/modular/sortable.complete.esm.js
         }
         const itemId = idMax + 1;
         const itemName = inputAdd.value;
+        //Si pas de thème sélectionné alors on ouvre la fenêtre modale de sélection et on attend sa résolution
         if (itemTheme === "All") {
             itemTheme = await openModal();
         }
-        console.log(itemTheme);
+        //Créer l'item dans le tableau items
         let item = new Item(itemId,itemName,itemTheme,false); 
         items.push(item);
         console.log("items :" , items);
 
-        //Appel à la fonction pour créer l'objet HTML
+        //Créer l'item HTML sur le DOM
         const itemHTML = createItemHTML(item, false, animate);
         list.insertAdjacentHTML("beforeend" , itemHTML);
         inputAdd.value = "";
     };
 
-    //Fonction : Ajouter l'objet item au DOM
+    //Fonction : créer l'objet item au DOM
     function createItemHTML(item, checked, animate) {
         let themeIcon;
         let themeClass;
         let checkClass = checked === true ? "check" : "";
-        //Les class et icones en fonction du thème
+
+        //Déterminer la class et l'icône de l'item en fonction du thème
         switch (item.theme) {
             case "Books" : 
                 themeIcon = "fas fa-book-open";
@@ -79,13 +89,15 @@ import Sortable from './node_modules/sortablejs/modular/sortable.complete.esm.js
             themeIcon = "fas fa-book-open";
             themeClass = "theme--books";
         };
-        //L'animation déclenchée ou non selon le clic
+
+        //Déclencher l'animation de création (uniquement à la création et non à la regénération de la liste avec un filtre)
         let animateFadeInUp;
         if (animate) {
             animateFadeInUp = "animate__fadeInUp";
         }
         else animateFadeInUp = "";
-            
+          
+        //Création du bloc HTML
         return `<div class="item animate__animated ${animateFadeInUp}" id="item${item.id}">
             <div class="item__check">
                 <input type="checkbox" ${item.checked ? 'checked' : ''}>
@@ -103,20 +115,25 @@ import Sortable from './node_modules/sortablejs/modular/sortable.complete.esm.js
             </div>
         </div>`;
     }
+
     //Event : cliquer sur le bouton Add
     btnAdd.addEventListener("click" , (e) => {
         e.preventDefault();
-        if (isModalOpen === true) { return };
-        console.log("btnListActive" , btnListActive)
+        if (isModalOpen === true) { return }; //Pas d'ajout pendant ouverture de la fenêtre modale
+
         if (inputAdd.value === "" && btnListActive === "On the list") {
             alert("Vous devez saisir quelque chose pour valider !")
             return
         }
-        else if (inputAdd.value === "" && btnListActive === "Completed") {
+        else if (inputAdd.value === "" && btnListActive === "Completed") { //Pas d'ajout pour la liste des items complétés
             return
         }
         const activeTheme = `${btnFilterActive}`;
         addItem(items, activeTheme, true);
+
+        //Enregistrer dans le local Storage
+        window.localStorage.setItem("items" , JSON.stringify(items));
+        console.log(JSON.stringify(items));
     });
 
     //Event : presser Entrée
@@ -134,6 +151,10 @@ import Sortable from './node_modules/sortablejs/modular/sortable.complete.esm.js
             }
             const activeTheme = `${btnFilterActive}`;
             addItem(items,activeTheme, true);
+
+            //Enregistrer dans le local Storage
+            window.localStorage.setItem("items" , JSON.stringify(items));
+            console.log(JSON.stringify(items))
         }
     })
 //#endregion
@@ -162,7 +183,6 @@ import Sortable from './node_modules/sortablejs/modular/sortable.complete.esm.js
         //Maj le tableau items
         let index = items.findIndex(item => item.id === id);
         items.splice(index, 1);
-        console.log(items);
         
         //Maj le DOM
         const itemHTMLRemove = document.querySelector(`#item${id}`);
@@ -180,11 +200,17 @@ import Sortable from './node_modules/sortablejs/modular/sortable.complete.esm.js
         elementId = parseInt(elementId.substr(4))
         if (e.target.tagName === "INPUT") { //Clic sur une checkbox
             checkItem(elementId);
-            console.log(items);
+
+            //Mettre à jour le localStorage
+            window.localStorage.setItem("items" , JSON.stringify(items));
+            console.log(JSON.stringify(items));
         }
         else if (e.target.tagName === "I") {
             removeItem(elementId); //Clic sur une corbeille
-            console.log(items);
+
+            //Mettre à jour le localStorage
+            window.localStorage.setItem("items" , JSON.stringify(items));
+            console.log(JSON.stringify(items));
         }
     });
 //#endregion
@@ -218,16 +244,19 @@ function openModal() {
     function updateList() {
         let filteredItems = items;
 
+        //Sélectionner les items en fonction de leur complétude
         if (btnListActive === "On the list") {
             filteredItems = filteredItems.filter(item => item.checked === false);
         } else if (btnListActive === "Completed") {
             filteredItems = filteredItems.filter(item => item.checked === true);
         }
-    
+        
+        //Sélection,er les items en fonction du thème (si all on prend tout)
         if (btnFilterActive !== "All") {
             filteredItems = filteredItems.filter(item => item.theme === btnFilterActive);
         }
 
+        //Regénérer la liste filtrée
         list.innerHTML = "";
         filteredItems.forEach(item => {
             const itemHTML = createItemHTML(item, item.checked, false);
@@ -235,8 +264,7 @@ function openModal() {
         });
     }
     
-    //Les boutons List
-    let btnListActive = "On the list";
+    //Gestion des deux boutons List
     let btnList = document.querySelectorAll(".btn__list");
     btnList.forEach(btn => {
         //Clic sur les boutons
@@ -252,7 +280,8 @@ function openModal() {
 
             //Appliquer le filtre sélectionné au DOM
             btnListActive = btn.value;
-            console.log("btnListActive" , btnListActive)
+
+            //Désactiver la barre de tâche pour la liste des items complétés
             if (btnListActive === "Completed") {
                 inputAdd.disabled = true;
                 btnAdd.disabled = true;
@@ -268,7 +297,6 @@ function openModal() {
     });
 
     //Les boutons Filtres
-    let btnFilterActive = "All";
     let btnFilters = document.querySelectorAll(".btn__filters");
     btnFilters.forEach(btn => {
         //Clic sur les boutons
@@ -285,7 +313,6 @@ function openModal() {
             //Appliquer le filtre sélectionné au DOM
             btnFilterActive = btn.value;
             updateList();
-            //! Pour ALL ajouter un tri par défaut sur le thème alphabetique
 
             //Mettre à jour le titre de la liste
             switch (btnFilterActive) {
